d69d1852cfbe226663eca5617e836063172e76d3
g0pika <71868599+g0pika@users.noreply.github.com>
2020-10-20 17:51:33 +0530

    Add files via upload

---
+++ C++/calculatorUsingSwitch.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    int num1, num2;
+    
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+    cout << "Enter the operator: ";
+    cin >> op;
+    
+    switch(op)
+    {
+        case '+':
+            cout <<num1<<" "<<op<<" "<<num2<<" = "<<num1+num2;
+            break;
+
+        case '-':
+            cout <<num1<<" "<<op<<" "<<num2<<" = "<< num1-num2;
+            break;
+
+        case '*':
+            cout <<num1<<" "<<op<<" "<<num2<<" = "<< num1*num2;
+            break;
+
+        case '/':
+            cout <<num1<<" "<<op<<" "<<num2<<" = "<< num1/num2;
+            break;
+
+        default:
+            cout << "\nInvalid Operator!";
+            break;
+    }
+
+    return 0;
+}
